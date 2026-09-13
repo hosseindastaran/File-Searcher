8949f0b643639b26c6b6622cedc8291bea405922
hossein dastaran <hosseindastaran1@gmail.com>
2026-09-13 13:50:58 +0330

    One step ahead

--- finder.c
+++ finder.c
@@ -12,8 +12,14 @@ int main(void)
         return 1;
     }
 
+    printf("Type what you want to search for : ");
+    char text[256];
+    fgets(text, 256, stdin);
+    
+    int newline_index = strcspn(text, "\n");
+    text[newline_index] = '\0';
+
     char line[256];
-    char text[] = "File";
     while (fgets(line,256,file) != NULL)
     {
         if (strstr(line, text) != NULL)
